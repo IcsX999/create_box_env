64e61be01b64c6ff4080d758faf3761f67d8f57f
root <root@localhost.localdomain>
2019-10-27 14:48:46 +0000

    add create_env script

---
+++ create_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "[*] Create ENV..."
+read -p "box's name: " box
+mkdir -p $box/user/enum/{nmap,dir}
+mkdir -p $box/root/enum
+touch $box/writeup.txt
+curl -s https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh -o $box/root/enum/LinEnum.sh
+echo "[+] DONE!"
