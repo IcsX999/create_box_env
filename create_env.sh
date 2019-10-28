efc6423bffde97c332e0611f2f1587c6e75560b7
IcsX999 <IcsX999>
2019-10-28 10:10:34 +0100

    create writeup file with title

--- create_env.sh
+++ create_env.sh
@@ -4,6 +4,6 @@ echo "[*] Create ENV..."
 read -p "box's name: " box
 mkdir -p $box/user/enum/{nmap,dir}
 mkdir -p $box/root/enum
-touch $box/writeup.txt
+echo -e "### $box writeup ###\n\n" > $box/writeup.txt
 curl -s https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh -o $box/root/enum/LinEnum.sh
 echo "[+] DONE!"
